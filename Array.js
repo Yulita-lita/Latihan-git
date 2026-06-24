a23e89afda28d985947d695260ee1bfec94b9697
Yulita <Yulitalita1506@gmail.com>
2026-06-24 14:42:33 +0800

    upload Array

---
+++ Array.js
@@ -0,0 +1,66 @@
+var angka = [2,5,1,3,4]
+console.log (angka [0])
+console.log (angka [2])
+
+var angka = [2,5,1,3,4]
+console.log (angka)
+
+
+// Method (Fungsi Bawaan)
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log (nama.length)
+
+// Length
+console.log (angka.length)
+
+
+//push 
+console.log ("sebelum Push :" + angka)
+angka.push (9)
+console.log ("setelah Push :" + angka)
+
+
+console.log ("sebelum pop:" + angka)
+angka.pop ()
+console.log ("setelah pop:" + angka)
+
+console.log ("sebelum unshif: " + angka)
+angka.unshif(8)
+console.log ("setelah unshif: " + angka)
+
+console.log ("sebelum shift: " + angka)
+angka.shift (9)
+console.log ("setelah shift: " + angka)
+
+
+
+
+var peserta = ["andra", "taufik", "adit", ]
+console.log (peserta)
+
+// Join 
+console.log ("sebelum join")
+console.log (peserta)
+// Peserta Join ("-")
+console.log ("setelah join")
+console.log(peserta.join("-"))
+
+
+// split 
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log ("sebelum split")
+console.log (nama)
+// Peserta Join ("-")
+console.log ("setelah di split")
+console.log(nama.split(""))
+
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log ("sebelum split")
+console.log (nama)
+// Peserta Join ("-")
+console.log ("setelah di split")
+console.log(nama.split(" "))
+
+
+var nama = "Muhammad Desta Greddy Aulia Rahman"
+console.log (nama.length)
