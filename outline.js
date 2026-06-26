650abaef4361ad71d9e10b606d85351614db5006
Yulita <Yulitalita1506@gmail.com>
2026-06-26 11:43:32 +0800

    upload outline

--- outline.js
+++ outline.js
@@ -8,19 +8,54 @@
 
 // Promise 
 
-var isMomHappy = false;
+// var isMomHappy = true;
 
-var willIGetNewPhone = new Promise(
-    function (resolove, reject) {
-        if (isMomHappy) {
-            var phone = {
-                brand: "Samsung",
-                color: "black"
-            };
-            resolove (phone); 
-        } else {
-            var reason = new Error ("mom is not happy"); 
-            reject (reason); 
+// var willIGetNewPhone = new Promise(
+//     function (resolve, reject) {
+//         if (isMomHappy) {
+//             var phone = {
+//                 brand: "Samsung",
+//                 color: "black"
+//             };
+//             resolve (phone); 
+//         } else {
+//             var reason = new Error ("mom is not happy"); 
+//             reject(reason); 
+//         }
+//     }
+// );
+// function askMom() {
+//     willIGetNewPhone
+//         .then (function (fulfilled) {
+//             console.log (fulfilled);
+//         })
+//         .catch (function (error) {
+//             console.log (error.message);
+//         });
+// }
+
+// askMom()
+
+
+function periksaDataPasien (nomorIdPasien) {
+    var dataPasien = [
+        {id : 1, nama: "john", jenisKelamin: "Laki-laki"},
+        {id : 2, nama: "Michael", jenisKelamin : "Laki-laki"}, 
+        {id : 3, nama: "Sarah", jenisKelamin : "Perempuan"},
+        {id : 4, nama: "Frank", jenisKelamin : "Laki-laki"},
+    ]
+    return new Promise (function (resolve, reject){
+        var pasien = dataPasien.find (x=> x.id === nomorIdPasien)
+        if (pasien === undefined){
+            reject ("data pasien tidak ada")
+        }else{
+            resolve (pasien)
         }
-    }
-)
+    })
+     
+}
+periksaDataPasien (5).then (function(data){
+    console.log (data)
+}).catch (function (err){
+    console.log (err)
+})
